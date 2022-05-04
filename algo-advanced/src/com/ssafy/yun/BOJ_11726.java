7594558cc81b09f4b7c54ceb266f829ff764e7e5
SmileJun <godhkekf24@inha.edu>
2022-05-04 21:40:49 +0900

    Feat: "2×n 타일링 1 문제풀이 "

---
+++ algo-advanced/src/com/ssafy/yun/BOJ_11726.java
@@ -0,0 +1,23 @@
+package com.ssafy.yun;
+
+import java.util.Scanner;
+
+public class BOJ_11726 {
+	static Scanner sc = new Scanner(System.in);
+	static int[] arr;
+	static int N;
+
+	public static void main(String[] args) {
+		N = sc.nextInt();
+		arr = new int[N + 1];
+		
+		arr[0] = 1;
+		arr[1] = 1;
+
+		for (int i = 2; i <= N; i++) {
+			arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+		}
+
+		System.out.println(arr[N]);
+	}
+}
